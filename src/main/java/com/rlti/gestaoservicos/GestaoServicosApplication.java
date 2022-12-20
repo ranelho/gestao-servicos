0239ea8807f2543b2f2b17dd84fd86628fa27def
ranelho <ranelhol@gmail.com>
2022-12-20 11:35:00 -0300

    inicializa projeto

---
+++ src/main/java/com/rlti/gestaoservicos/GestaoServicosApplication.java
@@ -0,0 +1,13 @@
+package com.rlti.gestaoservicos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GestaoServicosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GestaoServicosApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/rlti/gestaoservicos/GestaoServicosApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rlti.gestaoservicos;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GestaoServicosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
