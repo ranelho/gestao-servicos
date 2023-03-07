99b9cade1027deb39a758949ccdcb5414fbf73f3
ranelho <ranelhol@gmail.com>
2023-03-06 22:14:29 -0300

    implementa filtechain

--- src/main/java/com/rlti/gestaoservicos/configs/security/WebSecurityConfigV2.java
+++ src/main/java/com/rlti/gestaoservicos/configs/security/WebSecurityConfigV2.java
@@ -1,23 +1,18 @@
 package com.rlti.gestaoservicos.configs.security;
 
-import com.rlti.gestaoservicos.usuario.application.service.UserDetailsApplicationService;
-import lombok.AllArgsConstructor;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
-import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
 
-//@Configuration
-@AllArgsConstructor
-public class WebSecurityConfig extends WebSecurityConfigurerAdapter{
+@Configuration
+public class WebSecurityConfigV2 {
 
-    private final UserDetailsApplicationService userDetailsService;
-
-    @Override
-    protected void configure(HttpSecurity http) throws Exception {
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception{
         http
                 .httpBasic()
                 .and()
@@ -28,12 +23,7 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter{
                 .anyRequest().authenticated()
                 .and()
                 .csrf().disable();
-    }
-
-    @Override
-    protected void  configure(AuthenticationManagerBuilder auth) throws Exception{
-        auth.userDetailsService(userDetailsService)
-                .passwordEncoder(passwordEncoder());
+        return http.build();
     }
 
     @Bean
@@ -41,3 +31,4 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter{
         return new BCryptPasswordEncoder();
     }
 }
+
