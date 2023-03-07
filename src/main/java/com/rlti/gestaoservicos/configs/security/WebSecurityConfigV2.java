3e9943aeeaca7248da30ffa03691e8f42567bd9d
ranelho <ranelhol@gmail.com>
2023-03-06 22:34:37 -0300

    implementa validação

--- src/main/java/com/rlti/gestaoservicos/configs/security/WebSecurityConfigV2.java
+++ src/main/java/com/rlti/gestaoservicos/configs/security/WebSecurityConfigV2.java
@@ -2,13 +2,14 @@ package com.rlti.gestaoservicos.configs.security;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.http.HttpMethod;
+import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
+@EnableGlobalMethodSecurity(prePostEnabled = true)
 public class WebSecurityConfigV2 {
 
     @Bean
@@ -17,9 +18,6 @@ public class WebSecurityConfigV2 {
                 .httpBasic()
                 .and()
                 .authorizeRequests()
-                .antMatchers(HttpMethod.GET, "/v1/secretaria/**").permitAll()
-                .antMatchers(HttpMethod.POST, "/v1/secretaria").hasRole("USER")
-                .antMatchers(HttpMethod.DELETE, "/v1/secretaria/**").hasRole("ADMIN")
                 .anyRequest().authenticated()
                 .and()
                 .csrf().disable();
